a5d9535d9deb814bf73efc037befc9755de0edc7
Suryansh Singh <suryanshsingh1807@gmail.com>
2024-07-16 19:01:23 +0530

    Prettier Gud

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const paddle2 = document.getElementById("paddle2");
 const ball = document.getElementById("ball");
 const player1ScoreElement = document.getElementById("player1Score");
 const player2ScoreElement = document.getElementById("player2Score");
-const lossSound = document.getElementById('lossSound')
-const wallSound = document.getElementById('wallSound')
-const paddleSound = document.getElementById('paddleSound')
+const lossSound = document.getElementById("lossSound");
+const wallSound = document.getElementById("wallSound");
+const paddleSound = document.getElementById("paddleSound");
 
 // GAME VARIABLES
 let gameRunning = false;
@@ -30,153 +30,149 @@ const maxPaddleSpeed = 5;
 const gameHeight = 400;
 const gameWidth = 600;
 
-document.addEventListener("keydown",startGame)
-document.addEventListener("keydown",handleKeyDown)
-document.addEventListener("keyup",handleKeyUp)
+document.addEventListener("keydown", startGame);
+document.addEventListener("keydown", handleKeyDown);
+document.addEventListener("keyup", handleKeyUp);
 
 //start GAME
-function startGame(){
-    gameRunning = true;
-    startText.style.display = 'none';
-    document.removeEventListener('keydown',startGame);
-    gameLoop();
+function startGame() {
+  gameRunning = true;
+  startText.style.display = "none";
+  document.removeEventListener("keydown", startGame);
+  gameLoop();
 }
 
-function gameLoop(){
-    if (gameRunning){
-        updatePaddle1();
-        updatePaddle2();
-        moveBall();
-        setTimeout(gameLoop,8);
-    }
+function gameLoop() {
+  if (gameRunning) {
+    updatePaddle1();
+    updatePaddle2();
+    moveBall();
+    setTimeout(gameLoop, 8);
+  }
 }
 
 // KEY REGISTRATION
-function handleKeyDown(e){
-    keysPressed[e.key] = true;
+function handleKeyDown(e) {
+  keysPressed[e.key] = true;
 }
 
-function handleKeyUp(e){
-    keysPressed[e.key] = false;
+function handleKeyUp(e) {
+  keysPressed[e.key] = false;
 }
 
 // PADDLE MOVEMENT
-function updatePaddle1(){
-    if (keysPressed['w']){
-        paddle1Speed = Math.max(paddle1Speed-paddleAcceleration,-maxPaddleSpeed);
+function updatePaddle1() {
+  if (keysPressed["w"]) {
+    paddle1Speed = Math.max(paddle1Speed - paddleAcceleration, -maxPaddleSpeed);
+  } else if (keysPressed["s"]) {
+    paddle1Speed = Math.min(paddle1Speed + paddleAcceleration, maxPaddleSpeed);
+  } else {
+    if (paddle1Speed > 0) {
+      paddle1Speed = Math.max(paddle1Speed - paddleDecceleration, 0);
+    } else if (paddle1Speed < 0) {
+      paddle1Speed = Math.min(paddle1Speed + paddleDecceleration, 0);
     }
-    else if (keysPressed['s']){
-        paddle1Speed = Math.min(paddle1Speed+paddleAcceleration, maxPaddleSpeed);
-    }
-    else{
-        if (paddle1Speed>0){
-            paddle1Speed = Math.max(paddle1Speed-paddleDecceleration,0)
-        }
-        else if (paddle1Speed<0){
-            paddle1Speed = Math.min(paddle1Speed+paddleDecceleration,0)
-        }
-    }
-    paddle1Y += paddle1Speed
-    if (paddle1Y < 0){
-        paddle1Y = 0
-    }
+  }
+  paddle1Y += paddle1Speed;
+  if (paddle1Y < 0) {
+    paddle1Y = 0;
+  }
 
-    if (paddle1Y > gameHeight - paddle1.clientHeight){
-        paddle1Y = gameHeight - paddle1.clientHeight
-    }
-    paddle1.style.top = paddle1Y +'px'
+  if (paddle1Y > gameHeight - paddle1.clientHeight) {
+    paddle1Y = gameHeight - paddle1.clientHeight;
+  }
+  paddle1.style.top = paddle1Y + "px";
 }
 
-function updatePaddle2(){
-    if (keysPressed['ArrowUp']){
-        paddle2Speed = Math.max(paddle2Speed-paddleAcceleration,-maxPaddleSpeed);
+function updatePaddle2() {
+  if (keysPressed["ArrowUp"]) {
+    paddle2Speed = Math.max(paddle2Speed - paddleAcceleration, -maxPaddleSpeed);
+  } else if (keysPressed["ArrowDown"]) {
+    paddle2Speed = Math.min(paddle2Speed + paddleAcceleration, maxPaddleSpeed);
+  } else {
+    if (paddle2Speed > 0) {
+      paddle2Speed = Math.max(paddle2Speed - paddleDecceleration, 0);
+    } else if (paddle2Speed < 0) {
+      paddle2Speed = Math.min(paddle2Speed + paddleDecceleration, 0);
     }
-    else if (keysPressed['ArrowDown']){
-        paddle2Speed = Math.min(paddle2Speed+paddleAcceleration, maxPaddleSpeed);
-    }
-    else{
-        if (paddle2Speed>0){
-            paddle2Speed = Math.max(paddle2Speed-paddleDecceleration,0)
-        }
-        else if (paddle2Speed<0){
-            paddle2Speed = Math.min(paddle2Speed+paddleDecceleration,0)
-        }
-    }
-    paddle2Y += paddle2Speed
-    if (paddle2Y < 0){
-        paddle2Y = 0
-    }
+  }
+  paddle2Y += paddle2Speed;
+  if (paddle2Y < 0) {
+    paddle2Y = 0;
+  }
 
-    if (paddle2Y > gameHeight - paddle2.clientHeight){
-        paddle2Y = gameHeight - paddle2.clientHeight
-    }
-    paddle2.style.top = paddle2Y +'px'
+  if (paddle2Y > gameHeight - paddle2.clientHeight) {
+    paddle2Y = gameHeight - paddle2.clientHeight;
+  }
+  paddle2.style.top = paddle2Y + "px";
 }
 
 // BALL MOVEMENT
-function moveBall(){
-    ballX += ballSpeedX;
-    ballY += ballSpeedY;
-    //TOP-BOTTOM BORDER
-    if (ballY >= gameHeight-ball.clientHeight || ballY <= 0){
-        playSound(wallSound)
-        ballSpeedY = -ballSpeedY
-    }
-    //LEFT PADDLE HIT
-    if (ballX <= paddle1.clientWidth &&
-        ballY >= paddle1Y && 
-        ballY <= paddle1Y + paddle1.clientHeight
-    ){
-        playSound(paddleSound)
-        ballSpeedX = -ballSpeedX
-    }
-    //RIGHT PADDLE HIT
-    if (ballX >= gameWidth - paddle2.clientWidth - ball.clientWidth &&
-        ballY >= paddle2Y && 
-        ballY <= paddle2Y + paddle2.clientHeight
-    ){
-        playSound(paddleSound)
-        ballSpeedX = -ballSpeedX;
-    }
-    // RIGHT PLAYER SCORES
-    if (ballX <= 0){
-        player2Score++;
-        playSound(lossSound)
-        updateScoreBoard()
-        resetball()
-        pauseGame()
-    }
-    // LEFT PLAYER SCORES
-    else if (ballX >= gameWidth-ball.clientHeight ){
-        player1Score++;
-        playSound(lossSound)
-        updateScoreBoard()
-        resetball()
-        pauseGame()
-    } 
+function moveBall() {
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
+  //TOP-BOTTOM BORDER
+  if (ballY >= gameHeight - ball.clientHeight || ballY <= 0) {
+    playSound(wallSound);
+    ballSpeedY = -ballSpeedY;
+  }
+  //LEFT PADDLE HIT
+  if (
+    ballX <= paddle1.clientWidth &&
+    ballY >= paddle1Y &&
+    ballY <= paddle1Y + paddle1.clientHeight
+  ) {
+    playSound(paddleSound);
+    ballSpeedX = -ballSpeedX;
+  }
+  //RIGHT PADDLE HIT
+  if (
+    ballX >= gameWidth - paddle2.clientWidth - ball.clientWidth &&
+    ballY >= paddle2Y &&
+    ballY <= paddle2Y + paddle2.clientHeight
+  ) {
+    playSound(paddleSound);
+    ballSpeedX = -ballSpeedX;
+  }
+  // RIGHT PLAYER SCORES
+  if (ballX <= 0) {
+    player2Score++;
+    playSound(lossSound);
+    updateScoreBoard();
+    resetball();
+    pauseGame();
+  }
+  // LEFT PLAYER SCORES
+  else if (ballX >= gameWidth - ball.clientHeight) {
+    player1Score++;
+    playSound(lossSound);
+    updateScoreBoard();
+    resetball();
+    pauseGame();
+  }
 
-    ball.style.left = ballX + 'px';
-    ball.style.top = ballY + 'px';
+  ball.style.left = ballX + "px";
+  ball.style.top = ballY + "px";
 }
 
-function updateScoreBoard(){
-    player1ScoreElement.textContent = player1Score;
-    player2ScoreElement.textContent = player2Score;
+function updateScoreBoard() {
+  player1ScoreElement.textContent = player1Score;
+  player2ScoreElement.textContent = player2Score;
 }
 
-function resetball(){
-    ballX = gameWidth/2 - ball.clientWidth/2;
-    ballY = gameHeight/2 - ball.clientHeight/2;
-    ballSpeedX = Math.random() > 0.5 ? 2 : -2;
-    ballSpeedY = Math.random() > 0.5 ? 2 : -2;
+function resetball() {
+  ballX = gameWidth / 2 - ball.clientWidth / 2;
+  ballY = gameHeight / 2 - ball.clientHeight / 2;
+  ballSpeedX = Math.random() > 0.5 ? 2 : -2;
+  ballSpeedY = Math.random() > 0.5 ? 2 : -2;
 }
 
-function pauseGame(){
-    gameRunning = false
-    document.addEventListener('keydown',startGame)
+function pauseGame() {
+  gameRunning = false;
+  document.addEventListener("keydown", startGame);
 }
 
-function playSound(sound){
-    sound.currentTime = 0;
-    sound.play()
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
 }
