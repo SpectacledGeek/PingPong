5a45f41db01fc59d50b58f6261b57504567a4df9
Suryansh Singh <suryanshsingh1807@gmail.com>
2024-07-16 18:28:40 +0530

    Game Done Sound effect Remains

--- script.js
+++ script.js
@@ -1,1 +1,173 @@
-console.log("BAKA BAKA");
+//Create JS represention frm DOM
+const startText = document.getElementById("startText");
+const paddle1 = document.getElementById("paddle1");
+const paddle2 = document.getElementById("paddle2");
+const ball = document.getElementById("ball");
+const player1ScoreElement = document.getElementById("player1Score");
+const player2ScoreElement = document.getElementById("player2Score");
+
+// GAME VARIABLES
+let gameRunning = false;
+let keysPressed = {};
+let paddle1Speed = 0;
+let paddle1Y = 150;
+let paddle2Speed = 0;
+let paddle2Y = 150;
+let ballX = 290;
+let ballSpeedX = 2;
+let ballY = 190;
+let ballSpeedY = 2;
+let player2Score = 0;
+let player1Score = 0;
+
+//GAME CONTS
+const paddleAcceleration = 1;
+const paddleDecceleration = 1;
+const maxPaddleSpeed = 5;
+const gameHeight = 400;
+const gameWidth = 600;
+
+document.addEventListener("keydown",startGame)
+document.addEventListener("keydown",handleKeyDown)
+document.addEventListener("keyup",handleKeyUp)
+
+//start GAME
+function startGame(){
+    gameRunning = true;
+    startText.style.display = 'none';
+    document.removeEventListener('keydown',startGame);
+    gameLoop();
+}
+
+function gameLoop(){
+    if (gameRunning){
+        updatePaddle1();
+        updatePaddle2();
+        moveBall();
+        setTimeout(gameLoop,8);
+    }
+}
+
+// KEY REGISTRATION
+function handleKeyDown(e){
+    keysPressed[e.key] = true;
+}
+
+function handleKeyUp(e){
+    keysPressed[e.key] = false;
+}
+
+// PADDLE MOVEMENT
+function updatePaddle1(){
+    if (keysPressed['w']){
+        paddle1Speed = Math.max(paddle1Speed-paddleAcceleration,-maxPaddleSpeed);
+    }
+    else if (keysPressed['s']){
+        paddle1Speed = Math.min(paddle1Speed+paddleAcceleration, maxPaddleSpeed);
+    }
+    else{
+        if (paddle1Speed>0){
+            paddle1Speed = Math.max(paddle1Speed-paddleDecceleration,0)
+        }
+        else if (paddle1Speed<0){
+            paddle1Speed = Math.min(paddle1Speed+paddleDecceleration,0)
+        }
+    }
+    paddle1Y += paddle1Speed
+    if (paddle1Y < 0){
+        paddle1Y = 0
+    }
+
+    if (paddle1Y > gameHeight - paddle1.clientHeight){
+        paddle1Y = gameHeight - paddle1.clientHeight
+    }
+    paddle1.style.top = paddle1Y +'px'
+}
+
+function updatePaddle2(){
+    if (keysPressed['ArrowUp']){
+        paddle2Speed = Math.max(paddle2Speed-paddleAcceleration,-maxPaddleSpeed);
+    }
+    else if (keysPressed['ArrowDown']){
+        paddle2Speed = Math.min(paddle2Speed+paddleAcceleration, maxPaddleSpeed);
+    }
+    else{
+        if (paddle2Speed>0){
+            paddle2Speed = Math.max(paddle2Speed-paddleDecceleration,0)
+        }
+        else if (paddle2Speed<0){
+            paddle2Speed = Math.min(paddle2Speed+paddleDecceleration,0)
+        }
+    }
+    paddle2Y += paddle2Speed
+    if (paddle2Y < 0){
+        paddle2Y = 0
+    }
+
+    if (paddle2Y > gameHeight - paddle2.clientHeight){
+        paddle2Y = gameHeight - paddle2.clientHeight
+    }
+    paddle2.style.top = paddle2Y +'px'
+}
+
+// BALL MOVEMENT
+function moveBall(){
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+    //TOP-BOTTOM BORDER
+    if (ballY >= gameHeight-ball.clientHeight || ballY <= 0){
+        ballSpeedY = -ballSpeedY
+    }
+    //LEFT PADDLE HIT
+    if (ballX <= paddle1.clientWidth &&
+        ballY >= paddle1Y && 
+        ballY <= paddle1Y + paddle1.clientHeight
+    ){
+        ballSpeedX = -ballSpeedX
+    }
+    //RIGHT PADDLE HIT
+    if (ballX >= gameWidth - paddle2.clientWidth - ball.clientWidth &&
+        ballY >= paddle2Y && 
+        ballY <= paddle2Y + paddle2.clientHeight
+    ){
+        ballSpeedX = -ballSpeedX;
+    }
+    // RIGHT PLAYER SCORES
+    if (ballX <= 0){
+        player2Score++;
+        updateScoreBoard()
+        resetball()
+        pauseGame()
+    }
+    // LEFT PLAYER SCORES
+    else if (ballX >= gameWidth-ball.clientHeight ){
+        player1Score++;
+        updateScoreBoard()
+        resetball()
+        pauseGame()
+    } 
+
+    ball.style.left = ballX + 'px';
+    ball.style.top = ballY + 'px';
+}
+
+function updateScoreBoard(){
+    player1ScoreElement.textContent = player1Score;
+    player2ScoreElement.textContent = player2Score;
+}
+
+function resetball(){
+    ballX = gameWidth/2 - ball.clientWidth/2;
+    ballY = gameHeight/2 - ball.clientHeight/2;
+    ballSpeedX = Math.random() > 0.5 ? 2 : -2;
+    ballSpeedY = Math.random() > 0.5 ? 2 : -2;
+}
+
+function pauseGame(){
+    gameRunning = false
+    document.addEventListener('keydown',startGame)
+}
+
+function playSound(sound){
+    sound.currentTime = 0;
+}
